b9a1f3c19b745467681158b4406fb985fe32e0af
asharonbaltazar <asharonbaltazar@outlook.com>
2020-09-02 10:46:30 -0500

    Implemented local storage for caching

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -18,47 +18,61 @@ export const AppProvider = ({ children }) => {
   // Actions
   // Get people
   const getPeoplewithPosts = async (numPeople = 10, numPosts = 10) => {
-    // Create fake posts
-    const JSONposts = await axios.get(
-      `http://jsonplaceholder.typicode.com/posts?_limit=${numPosts * numPeople}`
-    );
+    let people;
 
-    // Create fake people
-    const people = [];
-    let comment_counter = 0;
-    for (let i = 1; i <= numPeople; i++) {
-      const person = await axios.get(
-        "https://randomuser.me/api/?nat=us,dk,fr,gb&inc=gender,name,picture"
+    // Check to see whether localStorage has people
+    if (localStorage.getItem("lorem-network")) {
+      people = JSON.parse(localStorage.getItem("lorem-network"));
+    } else {
+      people = [];
+
+      // Create fake posts
+      const JSONposts = await axios.get(
+        `http://jsonplaceholder.typicode.com/posts?_limit=${
+          numPosts * numPeople
+        }`
       );
 
-      // Filter out relevant posts to userId
-      let posts = JSONposts.data.filter((post) => {
-        return post.userId === i;
-      });
-
-      // Create fake comments to fake posts
-      const comments = [];
-      for (let j = 1; j <= numPosts; j++) {
-        comment_counter++;
-        let comment = await axios.get(
-          `https://jsonplaceholder.typicode.com/posts/${comment_counter}/comments`
+      // Create fake people
+      let comment_counter = 0;
+      for (let i = 1; i <= numPeople; i++) {
+        const person = await axios.get(
+          "https://randomuser.me/api/?nat=us,dk,fr,gb&inc=gender,name,picture"
         );
-        comments.push(comment.data.slice(0, Math.random() * (6 - 1) + 1));
+
+        // Filter out relevant posts to userId
+        let posts = JSONposts.data.filter((post) => {
+          return post.userId === i;
+        });
+
+        // Create fake comments to fake posts
+        const comments = [];
+        for (let j = 1; j <= numPosts; j++) {
+          comment_counter++;
+          let comment = await axios.get(
+            `https://jsonplaceholder.typicode.com/posts/${comment_counter}/comments`
+          );
+          comments.push(comment.data.slice(0, Math.random() * (6 - 1) + 1));
+        }
+
+        // Match fake posts with their respective fake comments
+        posts = posts.map((post, index) => {
+          return {
+            post,
+            comments: comments[index],
+          };
+        });
+
+        people.push({
+          ...person.data.results[0],
+          posts,
+        });
       }
 
-      // Match fake posts with their respective fake comments
-      posts = posts.map((post, index) => {
-        return {
-          post,
-          comments: comments[index],
-        };
-      });
-
-      people.push({
-        ...person.data.results[0],
-        posts,
-      });
+      // JSONify it for localStorage
+      localStorage.setItem("lorem-network", JSON.stringify(people));
     }
+
     dispatch({ type: "LOAD_PEOPLE", payload: people });
   };
 
